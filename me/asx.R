05fcf6c5205fe606d987569a3890c63d14134f03
Chris McKelt <chris@mckelt.com>
2016-08-20 20:51:27 +0800

    twitter analysis

---
+++ me/asx.R
@@ -0,0 +1,9 @@
+#https://www.r-bloggers.com/flipsider-support-for-asx-option-chain-data/
+rm(list = ls()) # clear vars
+library(devtools)
+#install_github('DataWookie/flipsideR')
+#install.packages("quantmod")
+library(flipsideR)
+
+WBC = getOptionChain("WBC", "ASX")
+head(WBC)
---
+++ me/twitter.R
@@ -0,0 +1,28 @@
+require(ROAuth)
+require(twitteR)
+library(RCurl)
+
+rm(list = ls())
+setwd("C:/dev/r-course/me")
+
+options(RCurlOptions = list(capath = system.file("CurlSSL", "cacert.pem", package = "RCurl"), ssl.verifypeer = FALSE))
+
+reqURL <- "https://api.twitter.com/oauth/request_token"
+
+accessURL <- "http://api.twitter.com/oauth/access_token"
+
+authURL <- "http://api.twitter.com/oauth/authorize"
+
+source("settings.R")
+
+download.file(url = "http://curl.haxx.se/ca/cacert.pem", destfile = "cacert.pem")
+
+setup_twitter_oauth(api_key, api_secret, access_token, access_token_secret)
+
+user <- twitteR::getUser("chris_mckelt")
+followers <- user$getFollowers()
+twitter_follower_names <- sapply(followers, FUN = function(row) paste(row$name, row$screenName, row$description, sep=" "))
+
+currentDate <- Sys.Date()
+csvFileName <- paste("twitter_follower_names_", currentDate, ".csv", sep = "")
+write.table(twitter_follower_names, csvFileName, sep = ",", col.names = FALSE, row.names = FALSE)
