395ad8e7eae5b76d870f14e702ef7728dd4c11f0
Chris McKelt <chris@mckelt.com>
2017-09-10 16:31:22 +0800

    clean up

--- 8-machine-learning/ml.R
+++ 8-machine-learning/ml.R
@@ -1,7 +1,30 @@
-install.packages("caret", dependencies = c("Depends", "Suggests"))
+source("c:/dev/r-course/include.r")
+using("caret")
+using("lubridate")
+using("pgmm")
+using("rpart")
+using("gbm")
+using("forecast")
+using("gbm")
+using("AppliedPredictiveModeling")
+using("ElemStatLearn")
 
-library(caret)
-# attach the iris dataset to the environment
-data(iris)
-# rename the dataset
-dataset <- iris
+ 
+data(vowel.train)
+data(vowel.test)
+
+vowel.train$y <- as.factor(vowel.train$y)
+vowel.test$y <- as.factor(vowel.test$y)
+set.seed(33833)
+modRF <- train(y ~ ., data = vowel.train, method = "rf") #, trControl=trainControl("cv"), number=3)
+modBoost <- train(y ~ ., data = vowel.train, method = "gbm", verbose = FALSE)
+
+predRF <- predict(modRF, vowel.test)
+predBoost <- predict(modBoost, vowel.test)
+agreedIndex <- predRF == predBoost
+
+cfmRf <- confusionMatrix(vowel.test$y, predRF)
+cfmBoost <- confusionMatrix(vowel.test$y, predBoost)
+cfmAgreed <- confusionMatrix(vowel.test$y[agreedIndex], predRF[agreedIndex])
+
+cfmRf$overall["Accuracy"]
--- include.r
+++ include.r
@@ -1,18 +1,24 @@
-# use("sqldf")
-use <- function(packageName) {
-    if (!require(packageName)) {
-        install.packages(dput(packageName))
-        library(packageName, character.only = TRUE)
+
+using <- function(packageName) {
+    if (!require(packageName, character.only = TRUE) && !(packageName %in% installed.packages())) {
+        install.packages(dput(packageName), dependencies = TRUE, quiet = FALSE)
     }
+
+   library(packageName, character.only = TRUE)
 }
 
 install_standard_packages <- function() {
     ## setup - install missing packages and reference
-    list.of.packages <- c("tidyverse", "knitr", "markdown", "moments", "e1071", "data.table", "sqldf", "downloader")
-    new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[, "Package"])]
-    if (length(new.packages)) install.packages(new.packages, dependencies = TRUE)
-    sapply(sapply(list.of.packages, library, character.only = TRUE, quietly = FALSE), require, character.only = TRUE, quietly = FALSE)
+    default.packages <- c("tidyverse", "knitr", "markdown", "moments", "e1071", "data.table", "sqldf", "downloader", "magrittr")
+    new.packages <- default.packages[!(default.packages %in% installed.packages()[, "Package"])]
+    for (pkg in new.packages) {
+           using(pkg)
+    }
+    #if (length(new.packages)) install.packages(new.packages, dependencies = TRUE)
+    #sapply(sapply(default.packages, library, character.only = TRUE, quietly = FALSE), require, character.only = TRUE, quietly = FALSE)
 }
+
+install_standard_packages()
 
 #' download and save file
 save_file = function(url, name) {
