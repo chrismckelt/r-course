2276b220e55a7b9ae8ddcfa41d25cf3dad86cd8f
Chris McKelt <chris@mckelt.com>
2017-09-12 22:48:38 +0800

    wip

--- 8-machine-learning/quiz-question-4.R
+++ 8-machine-learning/quiz-question-4.R
@@ -8,3 +8,19 @@ using("gbm")
 using("forecast")
 using("gbm")
 
+save_file("https://d396qusza40orc.cloudfront.net/predmachlearn/gaData.csv", "gaData.csv")
+
+library(lubridate) # For year() function below
+
+dat = read.csv("gaData.csv")
+training = dat[year(dat$date) < 2012,]
+testing = dat[(year(dat$date)) > 2011,]
+tstrain = ts(training$visitsTumblr)
+
+# BATS model (Exponential smoothing state space model with Box-Cox transformation, ARMA errors, Trend and Seasonal components)
+tsfit <- bats(tstrain)
+tsforecast <- forecast(tsfit, h = nrow(testing))
+results <- as.data.frame(tsforecast$lower[, 2])
+results[, 2] <- as.data.frame(tsforecast$upper[, 2])
+results[, 3] <- testing$visitsTumblr
+sum((results[3] > results[1]) & (results[3] < results[2])) / nrow(results)
