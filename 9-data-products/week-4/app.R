1cf8d7e7001fa3387da4488bfb6354781ec0578b
Chris_McKelt <chris@mckelt.com>
2017-10-14 23:29:30 +0800

    sat changes

--- 9-data-products/week-4/app.R
+++ 9-data-products/week-4/app.R
@@ -5,6 +5,7 @@ source("c:/dev/r-course/include.r")
 install_standard_packages()
 using("base64enc")
 #devtools::install_github("rstudio/shiny")
+using("RSQLite")
 using("shiny")
 using("choroplethr")
 using("choroplethrMaps")
@@ -13,42 +14,56 @@ using("readxl")
 using("devtools")
 using("ggplot2")
 using("plotly")
+devtools::install_github('rstudio/DT')
+library(DT)
 
 Sys.setenv("plotly_username" = "chrismckelt")
 Sys.setenv("plotly_api_key" = "M6S961nyr6MaEAwtNAM0")
 options(scipen = 999)
 set.seed(3333)
 
-loanbook <- read.csv("./data/loan.csv")
+
+if (!file.exists("./data/lending-club-loan-data.zip")) {
+    download.file("https://www.kaggle.com/wendykan/lending-club-loan-data/downloads/lending-club-loan-data.zip", "./data/lending-club-loan-data.zip")
+    unzip("./lending-club-loan-data.zip")
+}
+
+connection_string <- data1 <- paste(getwd(), '/data/database.sqlite', sep = '')
+db <- dbConnect(SQLite(), dbname = connection_string, loadable.extensions = TRUE, cache_size = NULL, synchronous = "off", flags = SQLITE_RWC, vfs = NULL)
+
+data.tables = dbListTables(db)
+data.loanbook <- sqldf("select * from loan LIMIT 100", connection = db)
+
 #loanbook <- read.csv.sql(file, sql = "select top 100 * from file", header = TRUE, sep = ",")
 ## load the state names
 data(state.regions)
 # merge the loan book with the state names
-loanbook <- merge(loanbook, state.regions, by.x = "addr_state", by.y = "abb")
+loanbook <- merge(data.loanbook, state.regions, by.x = "addr_state", by.y = "abb")
 
 ## clean
-dataDictionary <- read_excel("./data/LCDataDictionary.xlsx")
-dataDictionary <- sqldf("select * from dataDictionary where LoanStatNew != 'NA'")
+excel_file <- paste0(getwd(), "/data/LCdatadictionary.xlsx")
+data.dictionary <- read_excel(excel_file)
+data.dictionary <- sqldf("select * from [data.dictionary] where LoanStatNew != 'NA'")
 ### fields available in the data dictionary
-dd_names <- as.character(na.omit(dataDictionary$LoanStatNew))
+dd_names <- as.character(na.omit(data.dictionary$LoanStatNew))
 ### fields available in the loan book
-loanbook_names <- names(loanbook)
+view_names <- names(data.loanbook)
 ### show the fields described in data dictionary but not in the loan book
-setdiff(dd_names, loanbook_names)
+setdiff(dd_names, view_names)
 ### fix dates
-loanbook$issue_d <- as.Date(gsub("^", "01-", loanbook$issue_d), format = "%d-%b-%Y")
-loanbook$grade <- as.factor(loanbook$grade)
+data.loanbook$issue_d <- as.Date(gsub("^", "01-", data.loanbook$issue_d), format = "%d-%b-%Y")
+data.loanbook$grade <- as.factor(data.loanbook$grade)
 
 ### show the fields described in data dictionary but not in the loan book
-setdiff(dd_names, loanbook_names)
+setdiff(dd_names, view_names)
 
-# snapshot data
-data.amount <- loanbook %>%
+# snapshot data  tag_view_names
+view.amount <- loanbook %>%
   select(issue_d, loan_amnt) %>%
   group_by(issue_d) %>%
   summarise(Amount = sum(loan_amnt))
 
-data.loan_amount_by_grade <- sqldf("select sum(loan_amnt) as total_loan_amount, grade from [loanbook] group by grade")
+view.loan_amount_by_grade <- sqldf("select sum(loan_amnt) as total_loan_amount, grade from [data.loanbook] group by grade")
 #data.loan_amount_by_month_year <- sqldf("select sum(loan_amnt) as total_loan_amount, issue_d from [loanbook] group by issue_d")
 
 
@@ -57,18 +72,23 @@ data.loan_amount_by_grade <- sqldf("select sum(loan_amnt) as total_loan_amount, 
 
 server <- function(input, output, session) {
    
+    output$view_names <- renderDataTable({
+        datatable(view_names, options = list(orderClasses = TRUE, lengthMenu = c(5, 10, 30), pageLength = 5))
+    })
+   # session$selected.names <- renderDataTable(view_names)
+
     output$chartLoanAmount <- renderPlotly({
-        p <- plot_ly(data.loan_amount_by_grade, x = ~grade, y = ~total_loan_amount, colors = TRUE, type = 'bar',
-        marker = list(color = 'rgb(158,202,225)', line = list(color = 'rgb(8,48,107)', width = 1.5))) %>% 
+    p <- plot_ly(view.loan_amount_by_grade, x = ~grade, y = ~total_loan_amount, colors = TRUE, type = 'bar',
+        marker = list(color = 'rgb(158,202,225)', line = list(color = 'rgb(8,48,107)', width = 1.5))) %>%
         layout(title = "Loan amounts by credit grade quality",
-              autosize = F, 
-              width = 500, 
+              autosize = F,
+              width = 500,
               height = 350,
               xaxis = list(title = "Credit grade quality"),
               yaxis = list(title = "Total loan amount")
             )
-        return(p)
-        })
+    return(p)
+})
     
     
     observeEvent(input$do, {
@@ -80,14 +100,18 @@ server <- function(input, output, session) {
       session$sendCustomMessage(type = 'testmessage',
                                 message = list(a = 1, b = 'text',
                                                controller = input$controller))
+
+     on.exit(dbDisconnect(db), add = TRUE)
     })
     
 }
 
 ui = htmlTemplate("www/index.html",
-                    aboutTag = includeMarkdown("about.md"),
-                    footerTag = h3(format(Sys.Date(), format = "%a - %d %B, %Y")),
-                    chartLoanAmount = plotlyOutput("chartLoanAmount")
+                    tag_about = includeMarkdown("about.md"),
+                    tag_footer = h3(format(Sys.Date(), format = "%a - %d %B, %Y")),
+                    chart_loan_amount = plotlyOutput("chartLoanAmount"),
+                    tag_view_names = dataTableOutput("view_names")
                 )
 
 shinyApp(ui, server)
+
