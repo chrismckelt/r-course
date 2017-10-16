0b5e4ac0414be0118da42b9df21a22c9dbe0d1cc
Chris McKelt <chris@mckelt.com>
2017-10-16 20:07:25 +0800

    WIP

--- 9-data-products/week-4/app.R
+++ 9-data-products/week-4/app.R
@@ -1,6 +1,45 @@
-source("c:/dev/r-course/include.r")
+
+switch(Sys.info()[['sysname']],
+       Windows= {suppressMessages(setwd("C:/dev/r-course/9-data-products/week-4"))},
+       Linux  = {suppressMessages(setwd("~/week-4"))},
+       Darwin = {print("I'm a Mac.")})
+
+options(shiny.error = browser)
+options(shiny.reactlog = TRUE)
+using <- function(packageName) {
+  if (!require(packageName, character.only = TRUE) && !(packageName %in% installed.packages())) {
+    install.packages(dput(packageName), dependencies = TRUE, quiet = FALSE)
+  }
+  
+  library(packageName, character.only = TRUE)
+}
+
+using("pacman")
+
+install_standard_packages <- function() {
+  ## setup - install missing packages and reference
+  packs <- c("tidyverse", "knitr", "markdown", "moments", "e1071", "data.table", "sqldf", "downloader", "magrittr", "ggplot2", "lubridate")
+  p_load("foreach")
+  foreach(n = 1:length(packs)) %do%  using(packs[n])
+}
+
+#' download and save file
+save_file = function(url, name) {
+  if (!file.exists(name)) {
+    library(downloader)
+    download(url, destfile = name)
+  }
+}
+
+colString <- function() {
+  string40 <- "ncnnccnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn"
+  string80 <- "nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn"
+  string120 <- "nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn"
+  string160 <- "nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnc"
+  colString <- paste(string40, string80, string120, string160, sep = "")
+}
+
 install_standard_packages()
-suppressMessages(setwd("C:/dev/r-course/9-data-products/week-4"))
 using("RSQLite")
 using("shiny")
 using("shinyjs")
