7951a9f9efb00f1128cf954e4319d827faba73ff
Chris McKelt <chris@mckelt.com>
2017-10-31 11:23:44 +0800

    SAVEPOINT
    Former-commit-id: 3da93539d57363b0d335928125f6608bd91c7066

--- 10-capstone/milestone-report.R
+++ 10-capstone/milestone-report.R
@@ -1,12 +1,15 @@
 suppressMessages(rm(list = ls()))
 options(encoding = "UTF-8")
-#options(shiny.error = browser)
-options(shiny.reactlog = TRUE)
+
+options(stringsAsFactors = FALSE)
 source('c:/dev/r-course/10-capstone/include.r')
 library(pacman)
 p_load("tm")
 p_load("stringr")
-suppressMessages(setwd("c:/dev/r-course/10-capstone/week2"))
+p_load("readr")
+p_load("DescTools")
+library(tm)
+suppressMessages(setwd("c:/dev/r-course/10-capstone/"))
 
 download_zip_files <- function() {
     url <- "https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip"
@@ -16,9 +19,17 @@ download_zip_files <- function() {
     }
 }
 
+read_file <- function(path) {
+    con <- file(path)
+    data <- readLines(con)
+    close(con)
+    data
+}
+
 
 #download_zip_files()
+data.blogs <- read_file("final/en_US/en_US.blogs.txt")
+data.news <- read_file("final/en_US/en_US.news.txt")
+data.twitter <- read_file("final/en_US/en_US.twitter.txt")
 
-blogs <- readLines("../final/en_US/en_US.blogs.txt", encoding = "UTF-8")
-news <- readLines("../final/en_US/en_US.news.txt", encoding = "UTF-8")
-twitter <- readLines("../final/en_US/en_US.twitter.txt", encoding = "UTF-8")
+
